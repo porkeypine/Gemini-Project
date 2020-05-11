abb04de89d127af8f18dce7d588cc30cfa5468ec
theyellowfellow <reubentengjw@gmail.com>
2020-05-11 23:19:53 +0800

    random things (script to update image is in room.js, but its v rough right now)

---
+++ Assets/Scripts/Room.js
@@ -0,0 +1,5 @@
+var room = document.getElementById("castle");
+var roomPic = document.getElementById("castle-pic");
+room.addEventListener("click", function() {
+    roomPic.src = "Assets\\Images\\castle.jpg";
+});

709a6bd58682bea68a837680795d2a13e18509c7
zxrycyan <18YSHIH054Z@student.ri.edu.sg>
2020-05-15 17:41:32 +0800

    door ajar animation

--- assets/scripts/room.js
+++ assets/scripts/room.js
@@ -1,9 +1,24 @@
 $(document).ready(function(){
     $("#text1").fadeIn(1000);
     
-    $('.clickText').click(function(){
-        $("#text1").fadeOut(1000, function(){
+    $('#ct1').click(function(){
+        $("#text1").fadeOut(500, function(){
             $("#text2").fadeIn(500);
+            $(".door").fadeIn(0, function() {
+                $(".door").css("cursor", "pointer");
+
+            });
         });
     });
+    
+    $(".door").click(function() {
+        $("#text2").fadeOut(500);
+        $("#text3").delay(500).fadeIn(500);
+    });
+
+    $("#ct2").click(function(){
+        $(".background").hide(0);
+        $(".doorAjarBg").show(0);
+        $("#text3").hide(0);
+    })
 });

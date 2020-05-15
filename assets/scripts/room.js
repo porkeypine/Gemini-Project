c41cc3131af2d416aabde41f190016d0ae9b28ae
zxrycyan <18YSHIH054Z@student.ri.edu.sg>
2020-05-15 18:32:09 +0800

    some styling + into the second room

--- assets/scripts/room.js
+++ assets/scripts/room.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-    $("#text1").fadeIn(1000);
+    $("body").fadeIn(2000);
+
+    $("#text1").delay(1000).fadeIn(1000);
     
     $('#ct1').click(function(){
         $("#text1").fadeOut(500, function(){
             $("#text2").fadeIn(500);
             $(".door").fadeIn(0, function() {
                 $(".door").css("cursor", "pointer");
-
             });
         });
     });
@@ -20,5 +21,12 @@ $(document).ready(function(){
         $(".background").hide(0);
         $(".doorAjarBg").show(0);
         $("#text3").hide(0);
+    })
+
+    $(".doorAjar").click(function() {
+        $(".background").show(0);
+        $(".doorAjarBg").hide(0);
+        $(".door").hide(0);
+        $("#text4").delay(1000).fadeIn(500);
     })
 });

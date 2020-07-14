8e8bbb9d817b82174ae48bb2f14a67291ca701f3
zxrycyan <18YSHIH054Z@student.ri.edu.sg>
2020-07-15 06:32:59 +0800

    textttt

--- assets/scripts/story.js
+++ assets/scripts/story.js
@@ -15,14 +15,13 @@ let rightUnlocked = new Door("right");
 // prop: name, img, inspect_img, x, y, w, h
 // room: id, backgroundSrc, text, doors, props
 
-let Text0 = new Text("You wake up in an empty room.", "top: 10%", 0, false);
-let Text1 = new Text("Click me.", "", 0, true); 
-let Text2 = new Text("After click", "top: 50%", 1, false);
-
-let Room0 = new Room(0, "assets/Images/Rooms/OneDoorBackground.svg", [Text0, Text1, Text2], ["", centreUnlocked, ""]);
+let Text0_1 = new Text("You wake up in an empty room.", "top: 10%", 0, false);
+let Text0_2 = new Text("Click me.", "", 0, true); 
+let Text0_3 = new Text("After click", "top: 50%", 1, false);
+let Room0 = new Room(0, "assets/Images/Rooms/OneDoorBackground.svg", [Text0_1, Text0_2, Text0_3], ["", centreUnlocked, ""]);
 
 let lockedKeyDoor = new Door("right", true, "key 1");
-let key1 = new Prop("key 1", "assets/Images/Key.svg", "assets/images/key.svg", 727, 2178, 185, 169);
+let key1 = new Prop("key 1", "assets/images/Key.svg", "assets/images/KeyInspect.svg", 727, 2178, 185, 169);
 let Room1 = new Room(1, std_bg, document.getElementsByClassName("Room1"), [leftUnlocked, centreUnlocked, lockedKeyDoor], {"key 1": key1});
 
 let Room2 = new Room(2, std_bg, [], [leftUnlocked, centreUnlocked, rightUnlocked]);

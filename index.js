5243a18ad04cb8407358a438729c608bf1a3676c
zxrycyan <18YSHIH054Z@student.ri.edu.sg>
2020-07-15 18:16:22 +0800

    lasdjflka

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const path = require('path');
 
 app
     .use(express.static(__dirname + '/public'))
-    // .use('images', express.static(__dirname + '/assets/images'))
+    .use('images', express.static(__dirname + '/assets/images'))
     // .set('views', path.join(__dirname, 'views'))
     .get('/', (req, res) => {
-        // res.sendFile(path.join(__dirname, 'index.html'));
+        res.sendFile(path.join(__dirname, '/index.html'));
         // res.sendFile('index.html');
-        res.render('index')
+        // res.render('index')
     })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));

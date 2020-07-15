83d61852741502992c67f463d83ad97711c46fd9
zxrycyan <18YSHIH054Z@student.ri.edu.sg>
2020-07-15 18:00:46 +0800

    aksdjfklsa

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const PORT = process.env.PORT || 5000;
 const path = require('path');
 
 app
-    .use(express.static('public'))
-    // .get('/', (req, res) => {
-    //     res.sendFile(path.join(__dirname, 'public', 'game.html'));
-    // })
+    // .use(express.static('public'))
+    .get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));

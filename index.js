59994f56f01809480a22bc9120b47bd8a9f169fd
zxrycyan <18YSHIH054Z@student.ri.edu.sg>
2020-07-15 18:15:23 +0800

    blah

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const PORT = process.env.PORT || 5000;
 const path = require('path');
 
 app
-    // .use(express.static('public'))
+    .use(express.static(__dirname + '/public'))
+    // .use('images', express.static(__dirname + '/assets/images'))
+    // .set('views', path.join(__dirname, 'views'))
     .get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, 'index.html'));
+        // res.sendFile(path.join(__dirname, 'index.html'));
         // res.sendFile('index.html');
+        res.render('index')
     })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
